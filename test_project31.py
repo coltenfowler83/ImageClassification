314595dea3d9a9c4cbd862957daec4a6e12ae398
Colten Fowler <cofowle@microsoft.com>
2020-05-14 10:58:02 -0700

    Updated test

--- test_project31.py
+++ test_project31.py
@@ -9,3 +9,20 @@ import im_util
 plt.rcParams['figure.figsize'] = (16.0, 10.0)
 plt.rcParams['font.size'] = 16
 # force auto-reload of import modules before running code
+
+"""Load CIFAR10 data"""
+
+num_classes=10
+num_dims=32*32*3
+
+cifar10_names=['airplane', 'automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+
+num_train=49000
+num_valid=1000
+num_test=10000
+
+im_train,Y_train,im_valid,Y_valid,im_test,Y_test = data_util.get_CIFAR10_data(num_train,num_valid,num_test)
+
+X_train=np.reshape(im_train,(num_train,num_dims))
+X_valid=np.reshape(im_valid,(num_valid,num_dims))
+X_test=np.reshape(im_test,(num_test,num_dims))
